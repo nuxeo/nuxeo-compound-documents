07319ae4f77f814656e17ffd022ddafc1554a4a4
Kevin Leturc <kevin.leturc@hyland.com>
2025-03-17 15:37:13 +0100

    NXBT-3857: Update shared library

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -17,7 +17,7 @@
 *     Thomas Fowley
 *     Kevin Leturc <kevin.leturc@hyland.com>
 */
-library identifier: "platform-ci-shared-library@v0.0.39"
+library identifier: "platform-ci-shared-library@v0.0.49"
 
 Closure buildUnitTestStage(env) {
   return {
