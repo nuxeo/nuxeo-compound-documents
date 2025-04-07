d782bd2130a30622d6011fbcaa9556d801ea42b0
Kevin Leturc <kevin.leturc@hyland.com>
2025-04-07 11:17:43 +0200

    NXP-32428: Update shared library

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -17,7 +17,7 @@
 *     Thomas Fowley
 *     Kevin Leturc <kevin.leturc@hyland.com>
 */
-library identifier: "platform-ci-shared-library@v0.0.49"
+library identifier: "platform-ci-shared-library@v0.0.53"
 
 Closure buildUnitTestStage(env) {
   return {
